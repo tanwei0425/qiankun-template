7d6cb04f53fd59da6eefb14cdeefc09f37cfc509
tanwei@jilinxiangyun.com <tanwei@jilinxiangyun.com>
2023-02-16 17:04:35 +0800

    默认启用第应用

--- main/src/registerApps.js
+++ main/src/registerApps.js
@@ -1,25 +1,33 @@
-import { registerMicroApps, start } from 'qiankun'; // 底层是基于single-spa
+import { registerMicroApps, start, setDefaultMountApp } from 'qiankun'; // 底层是基于single-spa
 // ----- Step2 注册子应用
-registerMicroApps([
-    {
-        name: 'subReactApp',
-        entry: '//localhost:3001',
-        container: '#container',
-        activeRule: '/sub-react-app',
-    },
-    {
-        name: 'subReactApp2',
-        entry: '//localhost:3002',
-        container: '#container',
-        activeRule: '/sub-react-app2',
-    },
-    {
-        name: 'subVue3App',
-        entry: '//localhost:3003',
-        container: '#container',
-        activeRule: '/sub-vue3-app',
-    },
-],
+registerMicroApps(
+    [
+        {
+            name: 'subReactApp',
+            entry: '//localhost:3001',
+            container: '#container',
+            activeRule: '/sub-react-app',
+            loader: (loading) => {
+                console.log(loading, 'loading');
+            },
+            props: {
+                name: 'subReactApp',
+                test: 'testtesttest'
+            }
+        },
+        {
+            name: 'subReactApp2',
+            entry: '//localhost:3002',
+            container: '#container',
+            activeRule: '/sub-react-app2',
+        },
+        {
+            name: 'subVue3App',
+            entry: '//localhost:3003',
+            container: '#container',
+            activeRule: '/sub-vue3-app',
+        },
+    ],
     {
         beforeLoad: () => {
             console.log('加载前')
@@ -36,7 +44,13 @@ registerMicroApps([
         afterUnmount: () => {
             console.log('销毁后')
         },
-    })
+    }
+)
 
+
+// 设置主应用启动后默认进入的微应用。
+setDefaultMountApp('/sub-react-app2');
 // ----- Step3 启动应用
 start();
+
+
